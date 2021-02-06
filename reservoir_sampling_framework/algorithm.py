55370033d97c4915f943ccf6614eb749693068cc
uri-yanover <uri.yanover+github@gmail.com>
2021-02-06 09:02:03 +0200

    Added periodic logging

--- reservoir_sampling_framework/algorithm.py
+++ reservoir_sampling_framework/algorithm.py
@@ -1,7 +1,12 @@
 from typing import Generator, TypeVar, Generic, Callable, Tuple, Optional, Union, Iterator
 from dataclasses import dataclass
+from logging import getLogger
+
 from .interfaces import Sink
 
+
+_LOGGER = getLogger(__name__)
+_DEFAULT_LOGGING_PERIOD = 1 << 20
 
 T = TypeVar('T')
 F = TypeVar('F')
@@ -15,10 +20,14 @@ class SinkUsageIntent(Generic[T, F]):
 
 def reservoir_sample(source: Union[Iterator[T], Generator[T, None, None]], 
                      sink_usage_intents: Tuple[SinkUsageIntent[T, F], ...], 
-                     randrange: Callable[[int], int]) -> None:
+                     randrange: Callable[[int], int],
+                     logging_period=_DEFAULT_LOGGING_PERIOD) -> None:
     remaining = {}
 
     for (index, record) in enumerate(source, start=1):
+        if index % logging_period == 0:
+            _LOGGER.info(f'At index {index}. Size of remaining {len(remaining)}')
+
         if randrange(index) < len(sink_usage_intents):
             chosen = randrange(len(sink_usage_intents))
             if chosen in remaining:
@@ -33,5 +42,6 @@ def reservoir_sample(source: Union[Iterator[T], Generator[T, None, None]],
             else:
                 remaining[key] = count - 1
 
+    _LOGGER.info('Wrapping up')
     for key in remaining.keys():
         sink_usage_intents[key].sink.revert()
